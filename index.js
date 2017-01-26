1ebbd9ab96849734f3143d60e63d83d6c4c6edc8
James Quigley <jmquigley@outlook.com>
2017-01-26 13:50:55 -0600

    Simplifies xo configuration
    - Adds badges to the readme for code style and npm
    - Converted code to use opinionated xo styles

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ const _ = require('lodash');
 const objectAssign = require('object-assign');
 
 module.exports = function(msg, opts = undefined) {
-    opts = objectAssign({
-        chevron: '*',
-        log: console.log,
-        leadingLines: 2,
-        trailingLines: 1
-    }, opts);
+	opts = objectAssign({
+		chevron: '*',
+		log: console.log,
+		leadingLines: 2,
+		trailingLines: 1
+	}, opts);
 
-    let arr = [];
+	let arr = [];
 
-    if (opts.leadingLines < 0) {
-        opts.leadingLines = 0;
-    }
+	if (opts.leadingLines < 0) {
+		opts.leadingLines = 0;
+	}
 
-    if (opts.trailingLines < 0) {
-        opts.trailingLines = 0;
-    }
+	if (opts.trailingLines < 0) {
+		opts.trailingLines = 0;
+	}
 
-    _.times(Math.round(opts.leadingLines), function() {
-        arr.push('');
-    });
+	_.times(Math.round(opts.leadingLines), function() {
+		arr.push('');
+	});
 
-    arr.push(opts.chevron.repeat(msg.length));
-    arr.push(msg);
-    arr.push(opts.chevron.repeat(msg.length));
+	arr.push(opts.chevron.repeat(msg.length));
+	arr.push(msg);
+	arr.push(opts.chevron.repeat(msg.length));
 
-    _.times(Math.round(opts.trailingLines), function() {
-        arr.push('');
-    });
+	_.times(Math.round(opts.trailingLines), function() {
+		arr.push('');
+	});
 
-    if (opts.log) {
-        arr.forEach(function(line) {
-            opts.log(line);
-        });
-    }
+	if (opts.log) {
+		arr.forEach(function(line) {
+			opts.log(line);
+		});
+	}
 
-    return arr;
+	return arr;
 };
